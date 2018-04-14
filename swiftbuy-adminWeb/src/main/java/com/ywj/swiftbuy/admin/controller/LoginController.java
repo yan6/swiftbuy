90fc3336d1fb2cef832427dd7cb759e8fa14a208
ywj <yanwenjun@eyepetizer.net>
2018-04-14 10:53:21 +0800

    update 0414

--- swiftbuy-adminWeb/src/main/java/com/ywj/swiftbuy/admin/controller/LoginController.java
+++ swiftbuy-adminWeb/src/main/java/com/ywj/swiftbuy/admin/controller/LoginController.java
@@ -6,6 +6,8 @@ import com.ywj.swiftbuy.model.FailureAPIResponse;
 import com.ywj.swiftbuy.model.JacksonViews;
 import com.ywj.swiftbuy.model.SuccessAPIResponse;
 import com.ywj.swiftbuy.service.admin.AdminLoginService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,15 +19,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping(value = "/admin/login")
 public class LoginController {
 
+    private static final Logger LOG = LoggerFactory.getLogger(LoginController.class);
+
     @Autowired
     private AdminLoginService adminLoginService;
 
     @RequestMapping(value = "", method = RequestMethod.POST)
     @ResponseBody
     @JsonView(JacksonViews.Admin.class)
-    public APIResponse login(@RequestParam(value = "name",required = true)String name,
-                             @RequestParam(value = "password",required = true)String password){
-        return adminLoginService.login(name,password)?new SuccessAPIResponse():new FailureAPIResponse("用户名或密码错误");
+    public APIResponse login(@RequestParam(value = "name", required = true) String name,
+                             @RequestParam(value = "password", required = true) String password) {
+        LOG.info("name={},password={}", name, password);
+        return adminLoginService.login(name, password) ? new SuccessAPIResponse() : new FailureAPIResponse("用户名或密码错误");
     }
 
 
